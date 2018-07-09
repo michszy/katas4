43b595c755d231974205689b77f7f399e0c14180
Michel SZYCHIEWICZ <michszychiewicz@gmail.com>
2018-07-09 09:58:01 +0200

    katas

---
+++ katas4.py
@@ -0,0 +1,73 @@
+"""
+Placer ce fichier dans un dossier vide
+et initialiser un repertoire git dedans
+
+"""
+
+
+"""
+commit this file in git
+"""
+
+
+"""
+Matrice de rotation
+
+pour les matrices 2x2 les matrices de rotations sont
+de la forme suivante et comme leur nom l'indique ont 
+pour fonction de faire tourner les vecteurs : 
+
+    cos theta -sin theta
+    sin theta cos theta
+
+
+coder la fonction build_rotation_matrix(theta)
+
+qui retourne une matrice de rotation
+
+"""
+
+
+"""
+
+commit le fichier à nouveau
+
+"""
+
+"""
+
+Créer une branche "dev" avec la commande
+git checkout -b dev
+
+"""
+
+"""
+Afficher la matrice de rotation pour theta = pi / 2
+"""
+
+
+"""
+Calculer la multiplication entre la matrice de rotation pi / 2 et le vecteur
+[1 0] et le vecteur [0 1]
+
+"""
+
+
+"""
+commit le fichier
+
+"""
+
+
+"""
+retourner sur la branche master.
+vérifier que le fichier ne comporte pas les 
+modifications effectuées sur la branche dev
+"""
+
+"""
+fusionner les modifications de la branche dev
+dans la branche master 
+
+"""
+
